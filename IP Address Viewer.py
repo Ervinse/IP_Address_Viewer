de27e229c9b0512840b36f01c02ceb721c875541
Ervinse <Ervinse@foxmail.com>
2023-08-22 21:40:59 +0800

    新增复制ip地址功能

--- IP Address Viewer.py
+++ IP Address Viewer.py
@@ -22,23 +22,41 @@ def update_ips():
     public_ip_label.config(text="Public IP Address: " + get_public_ip())  # 更新公共IP地址标签的文本
 
 
+# 复制到剪切板的函数
+def copy_to_clipboard(content):
+    root.clipboard_clear()  # 清除剪切板内容
+    root.clipboard_append(content)  # 添加文本到剪切板
+    root.update()  # 更新tkinter界面
+
+
 root = tk.Tk()
 root.title("IP Address Viewer")  # 设置窗口标题
+root.minsize(320, 100)
 
-# 设置窗口的最小尺寸
-root.minsize(300, 100)
+# 设置较小的字体大小
+button_font = ("Arial", 8)
 
 # 创建并配置本地IP地址的标签
 local_ip_label = tk.Label(root, text="Local IP Address: ")
-local_ip_label.pack()
+local_ip_label.grid(row=0, column=0, sticky=tk.W, padx=10, pady=5)
+
+# 创建并配置复制本地IP地址的按钮
+copy_local_ip_button = tk.Button(root, text="Copy Local IP", height=1, font=button_font,
+                                 command=lambda: copy_to_clipboard(get_local_ip()))
+copy_local_ip_button.grid(row=0, column=1, padx=10, pady=5)
 
 # 创建并配置公共IP地址的标签
 public_ip_label = tk.Label(root, text="Public IP Address: ")
-public_ip_label.pack()
+public_ip_label.grid(row=1, column=0, sticky=tk.W, padx=10, pady=5)
+
+# 创建并配置复制公共IP地址的按钮
+copy_public_ip_button = tk.Button(root, text="Copy Public IP", height=1, font=button_font,
+                                  command=lambda: copy_to_clipboard(get_public_ip()))
+copy_public_ip_button.grid(row=1, column=1, padx=10, pady=5)
 
 # 创建并配置“更新IP”按钮，点击时会调用update_ips函数
 update_button = tk.Button(root, text="Update IPs", command=update_ips)
-update_button.pack()
+update_button.grid(row=2, column=0, columnspan=2, pady=10)
 
 update_ips()  # 初始运行时更新IP地址
 
